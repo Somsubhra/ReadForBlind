f932cb7adf2df0bae88d6d2a780dc2bf8bbe425a
Somsubhra <somsubhra.bairi@gmail.com>
2013-04-05 20:33:25 +0530

    Added documentation to OutputPage.xaml.cs

--- ReadForBlind/Views/OutputPage.xaml.cs
+++ ReadForBlind/Views/OutputPage.xaml.cs
@@ -14,6 +14,9 @@ using Windows.Phone.Speech.Synthesis;
 
 namespace ReadForBlind.Views
 {
+    /// <summary>
+    /// Class for the Output page for the app
+    /// </summary>
     public partial class OutputPage : PhoneApplicationPage
     {
         private String readText;
@@ -24,10 +27,13 @@ namespace ReadForBlind.Views
         SpeechSynthesizer synth;
         bool playing = false;
 
+        /// <summary>
+        /// The constructor of the Output page
+        /// </summary>
         public OutputPage()
         {
             InitializeComponent();
-
+           
             text = (List<String>)PhoneApplicationService.Current.State["text"];
             readText = String.Join(" ", text);
             txt.Text = readText;
@@ -37,20 +43,29 @@ namespace ReadForBlind.Views
             startConv();
         }
 
+        /// <summary>
+        /// Read the first speech of the output screen
+        /// </summary>
         private async void startConv()
         {
             reader.readText("May I reed the text for you? Tap the screen for yes");
         }
 
-        // this method should listen for the user input: play, pause, restart, etc..
+        /// <summary>
+        /// Event handler when the screen is double tapped (used for play, pause and restart, etc.)
+        /// </summary>
+        /// <param name="sender">The sender of the event</param>
+        /// <param name="e">The arguments passed to the event handler when the screen is double tapped</param>
         private void ScreenDoubleTap(object sender, System.Windows.Input.GestureEventArgs e)
         {
             throw new NotImplementedException();
         }
 
-        // Play pause the running speech
-        // if it's playing pause it
-        // if it's paused, play it.
+        /// <summary>
+        /// Event handler when screen is tapped (used for play/pause)
+        /// </summary>
+        /// <param name="sender">The sender of the event</param>
+        /// <param name="e">The arguments passed to the event handler when the screen is tapped</param>
         private void ScreenTap(object sender, System.Windows.Input.GestureEventArgs e)
         {
             if (!playing)
@@ -59,6 +74,9 @@ namespace ReadForBlind.Views
                 PauseText();
         }
 
+        /// <summary>
+        /// Plays the text
+        /// </summary>
         private void PlayText()
         {
             reader.CancelAll();
@@ -77,6 +95,11 @@ namespace ReadForBlind.Views
             playing = true;
         }
 
+
+        /// <summary>
+        /// Makes an SSML of the text to be spoken
+        /// </summary>
+        /// <returns>The SSML of the text</returns>
         private string makeSSML()
         {
             if (count == maxTextLength)
@@ -95,6 +118,11 @@ namespace ReadForBlind.Views
             return s;
         }
 
+        /// <summary>
+        /// Event handler when a bookmark is reached
+        /// </summary>
+        /// <param name="sender">The sender of the event</param>
+        /// <param name="e">The arguments passed to the event handler when bookmark is reached</param>
         private async void synth_BookmarkReached(SpeechSynthesizer sender, SpeechBookmarkReachedEventArgs e)
         {
             count++;
@@ -107,11 +135,20 @@ namespace ReadForBlind.Views
             }
         }
 
+        /// <summary>
+        /// Pauses the text
+        /// </summary>
         private void PauseText()
         {
             if (synth != null) synth.Dispose();
             playing = false;
         }
+
+        /// <summary>
+        /// Reads the text
+        /// </summary>
+        /// <param name="token">The cancelleation token</param>
+        /// <returns>The task of reading the text</returns>
         private async Task ReadText(CancellationToken token)
         {
             for (; count < text.Count; count++)
@@ -121,6 +158,10 @@ namespace ReadForBlind.Views
             }
         }
 
+        /// <summary>
+        /// Event handler when the app navigates away from the Output Page
+        /// </summary>
+        /// <param name="e">The arguments passed to the event handler when the output page is navigated away from</param>
         protected override void OnNavigatedFrom(NavigationEventArgs e)
         {
             try
@@ -132,6 +173,11 @@ namespace ReadForBlind.Views
                 
         }
 
+
+        /// <summary>
+        /// Event handler when the app navigates to the output page
+        /// </summary>
+        /// <param name="e">The arguments passed to the event handler when the app navigated to the output page</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             NavigationService.RemoveBackEntry();
@@ -144,6 +190,11 @@ namespace ReadForBlind.Views
         //    NavigationService.GoBack();
         //}
 
+        /// <summary>
+        /// Event handler to open the voice commands for the page
+        /// </summary>
+        /// <param name="sender">The sender of the event</param>
+        /// <param name="e">The arguments passed to the event handler when the voice command is opened</param>
         private async void OpenVoiceCommand(object sender, System.Windows.Input.GestureEventArgs e)
         {
             string result = await listener.Listen();
