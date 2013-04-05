cfd2c274daa5f5a064b855ee96906f0528870e76
Somsubhra <somsubhra.bairi@gmail.com>
2013-04-05 20:42:36 +0530

    Added documentation for Reader.cs

--- ReadForBlind/Reader.cs
+++ ReadForBlind/Reader.cs
@@ -7,30 +7,52 @@ using Windows.Phone.Speech.Synthesis;
 
 namespace ReadForBlind
 {
+    /// <summary>
+    /// The wrapper class for speech generation
+    /// </summary>
     class Reader
     {
         private SpeechSynthesizer reader;
 
+        /// <summary>
+        /// The constructor for the reader
+        /// </summary>
         public Reader()
         {
             reader = new SpeechSynthesizer();
         }
 
+
+        /// <summary>
+        /// Read the specified text
+        /// </summary>
+        /// <param name="text">The text to be read</param>
+        /// <returns>The task of reading the text</returns>
         public async Task readText(string text)
         {
             await reader.SpeakTextAsync(text);
         }
 
+        /// <summary>
+        /// Reads the welcome text
+        /// </summary>
         public void readWelcomeText()
         {
             string welcomeText = "Welcome. You may now hold your phone over the text for me to reader it aloud.";
             readText(welcomeText);
         }
 
+
+        /// <summary>
+        /// Disposes the reader object
+        /// </summary>
         public void Dispose() {
             reader.Dispose();
         }
 
+        /// <summary>
+        /// Cancels all the tasks of the reader object
+        /// </summary>
         public void CancelAll() {
             reader.CancelAll();
         }
